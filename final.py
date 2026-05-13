1d5e60d08a1061a5bf08ab682d9cef1393ff5a1f
abishek2006paul-afk <abishek2006paul@gmail.com>
2026-05-13 16:27:43 +0530

    Update final.py

--- final.py
+++ final.py
@@ -357,7 +357,7 @@ st.markdown("""
 @st.cache_resource
 def load_model_and_data():
     try:
-        df = pd.read_csv(r"C:\Users\Admin\Desktop\python basic\project weather ass\air.csv")
+        df = pd.read_csv("./air.csv")
         df.columns = df.columns.str.strip()
         num_cols = df.select_dtypes(include=[np.number]).columns.tolist()
         df[num_cols] = df[num_cols].fillna(df[num_cols].mean())
